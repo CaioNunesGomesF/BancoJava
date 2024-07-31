0482920a1203cac460930f5d55f9f4c565f67e44
Caio Nunes <caiongomes@gmail.com>
2024-07-31 14:14:28 -0300

    first commit

---
+++ src/Program/Main.java
@@ -0,0 +1,23 @@
+package Program;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cliente venilton = new Cliente();
+        venilton.setNome("Venilton");
+
+        Conta cc = new ContaCorrente(venilton);
+        Conta poupanca = new ContaPoupanca(venilton);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
+
+}
